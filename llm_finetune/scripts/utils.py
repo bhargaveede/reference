mlcommons/training

fixing dataset and model links and updating bash script and readme

llm_finetune/scripts/utils.py
@@ -277,7 +277,7 @@ def create_and_prepare_model(args):
     device_map = None
 
     model = AutoModelForCausalLM.from_pretrained(
-        'regisss/llama2-70b-fused-qkv-mlperf',
+        args.model_path,
         device_map=device_map,
         use_cache=not args.use_gradient_checkpointing,
         trust_remote_code=True,

bhargaveede/reference

llm_finetune/scripts/utils.py
@@ -306,7 +306,7 @@ def create_and_prepare_model(script_args, training_args):
         device_map = "auto"  # {"": 0}
 
     model = AutoModelForCausalLM.from_pretrained(
-        'regisss/llama2-70b-fused-qkv-mlperf',
+        script_args.model_path,
         load_in_8bit=load_in_8bit,
         quantization_config=bnb_config,
         device_map=device_map,